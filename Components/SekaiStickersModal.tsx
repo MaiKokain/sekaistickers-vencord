4eb14261cec5a87b3f7e7ca39a8c53302a223874
yuria <yuuria_sakura@proton.me>
2024-03-20 16:45:32 +0700

    Fix rotation and few minor changes

--- Components/SekaiStickersModal.tsx
+++ Components/SekaiStickersModal.tsx
@@ -7,7 +7,7 @@
 import { Flex } from "@components/Flex";
 import { ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
 import { findByProps } from "@webpack";
-import { Button, ChannelStore, Forms, React, Slider, Switch, Text, TextInput, UploadHandler } from "@webpack/common";
+import { Button, ChannelStore, Forms, React, Slider, Switch, Text, TextArea, UploadHandler } from "@webpack/common";
 
 import { characters } from "../characters.json";
 import Canvas from "./Canvas";
@@ -16,20 +16,25 @@ import CharSelectModal from "./Picker";
 export default function SekaiStickersModal({ modalProps, settings }: { modalProps: ModalProps; settings: any; }) {
     const [text, setText] = React.useState<string>("奏でーかわいい");
     const [character, setChracter] = React.useState<number>(49);
-    const [fontSize, setFontSize] = React.useState<number>(30);
-    const [rotate, setRotate] = React.useState<number>(0);
+    const [fontSize, setFontSize] = React.useState<number>(characters[character].defaultText.s);
+    const [rotate, setRotate] = React.useState<number>(characters[character].defaultText.r);
     const [curve, setCurve] = React.useState<boolean>(false);
     const [isImgLoaded, setImgLoaded] = React.useState<boolean>(false);
-    const [position, setPosition] = React.useState<{ x: number, y: number; }>({ x: 148, y: 56 });
-    const [spaceSize, setSpaceSize] = React.useState<number>(30);
+    const [position, setPosition] = React.useState<{ x: number, y: number; }>({ x: characters[character].defaultText.x, y: characters[character].defaultText.y });
+    const [spaceSize, setSpaceSize] = React.useState<number>(1);
     let canvast!: HTMLCanvasElement;
     const img = new Image();
     img.crossOrigin = "anonymous";
     img.src = "https://st.ayaka.one/img/" + characters[character].img;
 
     React.useEffect(() => {
+        setPosition({
+            x: characters[character].defaultText.x,
+            y: characters[character].defaultText.y
+        });
+        setFontSize(characters[character].defaultText.s);
+        setRotate(characters[character].defaultText.r);
         setImgLoaded(false);
-        setPosition({ x: 148, y: 56 });
     }, [character]);
 
     img.onload = () => { setImgLoaded(true); };
@@ -60,27 +65,28 @@ export default function SekaiStickersModal({ modalProps, settings }: { modalProp
             ctx.font = `${fontSize}px YurukaStd, SSFangTangTi`;
             ctx.lineWidth = 9;
             ctx.save();
+
             ctx.translate(position.x, position.y);
             ctx.rotate(rotate / 10);
             ctx.textAlign = "center";
             ctx.strokeStyle = "white";
             ctx.fillStyle = characters[character].color;
+            const lines = text.split("\n");
             if (curve) {
-                let k = 0;
-                for (let i = 0; i < text.length; i++) {
-                    ctx.rotate(angle / text.length / 2.5);
-                    ctx.save();
-                    ctx.translate(0, -1 * fontSize * 3.5);
-                    ctx.strokeText(text[i], 0, -1 * spaceSize);
-                    ctx.fillText(text[i], 0, -1 * spaceSize);
-                    k += spaceSize;
-                    ctx.restore();
+                for (const line of lines) {
+                    for (let i = 0; i < line.length; i++) {
+                        ctx.rotate(angle / line.length / 2.5);
+                        ctx.save();
+                        ctx.translate(0, -1 * fontSize * 3.5);
+                        ctx.strokeText(line[i], 0, -1 * spaceSize);
+                        ctx.fillText(line[i], 0, -1 * spaceSize);
+                        ctx.restore();
+                    }
                 }
             } else {
-                let k = 0;
-                for (let i = 0; i < text.length; i++) {
-                    ctx.strokeText(text[i], 0, k);
-                    ctx.fillText(text[i], 0, k);
+                for (let i = 0, k = 0; i < lines.length; i++) {
+                    ctx.strokeText(lines[i], 0, k);
+                    ctx.fillText(lines[i], 0, k);
                     k += spaceSize;
                 }
                 ctx.restore();
@@ -105,13 +111,13 @@ export default function SekaiStickersModal({ modalProps, settings }: { modalProp
                     </div>
                     <div style={{ marginRight: 10, width: "30vw" }}>
                         <Forms.FormTitle>Text</Forms.FormTitle>
-                        <TextInput onChange={(e: string) => { setText(e); }} content="奏でーかわいい" placeholder="奏でーかわいい" />
+                        <TextArea onChange={setText} placeholder={text} rows={4} spellCheck={false} />
                         <Forms.FormTitle>Rotation</Forms.FormTitle>
-                        <Slider minValue={-10} maxValue={10} asValueChanges={val => setRotate(Math.round(val))} initialValue={rotate} keyboardStep={0.2} orientation={"horizontal"} onValueRender={(v: number) => String(Math.round(v))} />
+                        <Slider markers={[-10, -5, 0, 5, 10]} stickToMarkers={false} minValue={-10} maxValue={10} asValueChanges={val => setRotate(val)} initialValue={rotate} keyboardStep={0.2} orientation={"horizontal"} onValueRender={(v: number) => String(v.toFixed(2))} />
                         <Forms.FormTitle>Font Size</Forms.FormTitle>
                         <Slider minValue={10} asValueChanges={val => setFontSize(Math.round(val))} maxValue={100} initialValue={fontSize} keyboardStep={1} orientation={"horizontal"} onValueRender={(v: number) => String(Math.round(v))} />
                         <Forms.FormTitle>Spacing</Forms.FormTitle>
-                        <Slider minValue={18} maxValue={100} initialValue={spaceSize} asValueChanges={e => setSpaceSize(Math.round(e))} onValueRender={e => String(Math.round(e))} />
+                        <Slider markers={[18, 36, 72, 100]} stickToMarkers={false} minValue={18} maxValue={100} initialValue={spaceSize} asValueChanges={e => setSpaceSize(e)} onValueRender={e => String(Math.round(e))} />
                         <Switch value={curve} onChange={val => setCurve(val)}>Enable curve</Switch>
                         <Button onClick={() => openModal(props => <CharSelectModal modalProps={props} setCharacter={setChracter} />)}>Switch Character</Button>
                     </div>
